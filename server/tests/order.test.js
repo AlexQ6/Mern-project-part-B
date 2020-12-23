b9857ac2d0d9591419c80e6515525c94adc74a09
Alex Qiang <alex.qiang36@gmail.com>
2020-12-23 14:54:59 +1100

    Add order tests

--- server/tests/order.test.js
+++ server/tests/order.test.js
@@ -4,6 +4,10 @@ const app = require("../server");
 const mongoose = require("mongoose");
 const Order = require("../models/Order");
 
+// * CHAI CONFIG
+chai.use(chaiHttp);
+chai.should();
+
 // * TEST SETUP
 const testMongoUri = "mongodb://localhost/cafe_app_test";
 
@@ -31,3 +35,75 @@ const connectToDb = (done) => {
     }
   );
 };
+
+
+// Create test Order document
+
+const setupData = () => {
+    const name = "Bob"
+    const mobile = "0400 000 000";
+    const email = "bob@gmail.com";
+    const items = [{item: "sausage roll", price: 3, quantity: 2, options: ["extra sauce"] }];
+    const totalPrice = 6
+    const newOrder = {name, mobile, email, items, totalPrice}
+    return Order.create(newOrder)
+};
+
+// Before each test create a new Order document
+beforeEach(async function(){
+    const order = await setupData();
+    orderId = order._id
+
+})
+
+// Delete Order document
+function tearDownData() {
+	return Post.deleteMany()
+}
+
+
+// Delete test data after each test
+afterEach((done) => {
+    // Execute the deleteMany query
+    tearDownData().exec(() => done());
+});
+
+// Close the connection to the db once all the tests have been run
+after((done) => {
+    mongoose.disconnect(() => done());
+});
+
+describe("order CURD operations", function () {
+    describe("get all orders at /orders", function (){
+        it("should return with status 200", function () {
+            chai
+                .request(app)
+                .get("/orders")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                });
+        })
+
+        it("should return an array", function () {
+            chai
+                .request(app)
+                .get("/orders")
+                .end((err, res) => {
+                    res.should.be.a("array");
+                });
+        });
+    });
+
+    describe("get one order at orders/:id", function () {
+        it("should return one order with orderId", function () {
+            chai
+                .request(app)
+                .get(`orders/${OrderId}`)
+                .end((err, res) => {
+                    const name = res.body.name
+                    name.should.equal("Bob")
+                })
+        })
+    })
+});
+
